96fe9bfa6cea3e12df4da999f7f2b8ab1c7ca82c
OkoliDaniel <okolidaniel57@gmail.com>
2025-03-07 00:07:02 +0100

    change avatar_hash in table definition to type VARCHAR(64)

--- sql/schema/001_whitelistMinters.sql
+++ sql/schema/001_whitelistMinters.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS whitelistMinters (
     discord_id VARCHAR(64),
     discord_username VARCHAR(64),
     wallet_address CHAR(42) UNIQUE,
-    avatar_hash char(64) UNIQUE,
+    avatar_hash VARCHAR(64) UNIQUE,
     nonce SMALLINT NOT NULL UNIQUE,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
